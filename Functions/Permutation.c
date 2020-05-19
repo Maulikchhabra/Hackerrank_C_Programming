00cc8c9be31accccf161b59ba2d9a7e54e34c31e
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2020-05-19 10:34:42 +0530

    Permutations of Strings problem

---
+++ Functions/Permutation.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int next_permutation(int n, char **s)
+{
+	/**
+	* Complete this method
+	* Return 0 when there is no next permutation and 1 otherwise
+	* Modify array s to its next permutation
+	*/
+    if(n<=1){
+        return 0;
+    }
+
+    int i = n-1;
+    while(i>0 && strcmp(s[i-1],s[i]) >=0){
+        i--;
+    }
+
+    if(i==0){
+        return 0;
+    }
+
+    int j =n-1;
+    while(strcmp(s[j], s[i-1])<=0){
+        j--;
+    }
+
+    char *temp = s[i - 1];
+    s[i - 1] = s[j];
+    s[j] = temp;
+
+    j = n - 1;
+    while (i < j)
+    {
+        temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+        i++;
+        j--;
+    }
+
+    return 1;
+
+}
+
+int main()
